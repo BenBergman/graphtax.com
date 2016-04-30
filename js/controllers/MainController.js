fef2729957518cb96306629adfdd244651bea9eb
Benjamin Bergman <ben@benbergman.ca>
2016-04-30 15:50:40 -0500

    Minimal D3/Angular integration example from https://github.com/haroldcampbell/d3_angular_example_1

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,17 @@
 app.controller("MainController", ["$scope", function($scope) {
     $scope.title = "Foobar";
+    $scope.updateChart = function() {
+        var index = 0;
+        var data = angular.fromJson("[" + $scope.chart_data + "]");
+        var bar_width = 500 / (data.length);
+
+        d3.selectAll('.chart').selectAll('div').remove();
+        d3.select(".chart")
+            .selectAll("div")
+            .data(data)
+            .enter().append("div")
+            .style("width", function(d) { return bar_width + "px"; })
+            .style("height", function(d) { return d + "%"; })
+            .style("left", function(d) { return (index++) * (bar_width + 2) + "px"; });
+    };
 }]);
