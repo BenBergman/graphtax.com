fd906a0db7bce1056a4d5958137b1278e4f66786
Benjamin Bergman <ben@benbergman.ca>
2016-04-30 21:33:25 -0500

    A bit of tidying

--- js/directives/taxChart.js
+++ js/directives/taxChart.js
@@ -3,7 +3,7 @@ app.directive('taxChart', function() {
         restrict: 'E',
         scope: {},
         link: function(scope, element, attrs) {
-            d3.select(element[0])
+            var svg = d3.select(element[0])
                 .append("svg")
                 .attr("id", "directive")
                 .style({
@@ -102,93 +102,89 @@ app.directive('taxChart', function() {
                 .y(function(d) { return yScale2(d["Marginal Rate"]); })
                 .interpolate("basis");
 
-            function InitChart(data) {
-                var vis = d3.select("#directive");
 
-                vis.append("svg:g")
-                    .attr("class", "x axis")
-                    .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
-                    .call(xAxis);
-                vis.append("svg:g")
-                    .attr("class", "y axis")
-                    .attr("transform", "translate(" + (WIDTH - MARGINS.right) + ",0)")
-                    .call(yAxis);
-                vis.append("svg:g")
-                    .attr("class", "y axis")
-                    .attr("transform", "translate(" + (MARGINS.left) + ",0)")
-                    .call(yAxis2);
+            svg.append("svg:g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+                .call(xAxis);
+            svg.append("svg:g")
+                .attr("class", "y axis")
+                .attr("transform", "translate(" + (WIDTH - MARGINS.right) + ",0)")
+                .call(yAxis);
+            svg.append("svg:g")
+                .attr("class", "y axis")
+                .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+                .call(yAxis2);
 
-                vis.selectAll("line.horizontalGrid").data(yScale2.ticks(4)).enter()
-                    .append("line")
-                    .attr({
-                        "class": "horizontalGrid",
-                        "x1": MARGINS.right,
-                        "x2": WIDTH - MARGINS.right,
-                        "y1": function(d) { return yScale2(d); },
-                        "y2": function(d) { return yScale2(d); },
-                        "fill": "none",
-                        "shape-rendering": "crispEdges",
-                        "stroke": "grey",
-                        "stroke-width": "1px"
-                    });
+            svg.selectAll("line.horizontalGrid").data(yScale2.ticks(4)).enter()
+                .append("line")
+                .attr({
+                    "class": "horizontalGrid",
+                    "x1": MARGINS.right,
+                    "x2": WIDTH - MARGINS.right,
+                    "y1": function(d) { return yScale2(d); },
+                    "y2": function(d) { return yScale2(d); },
+                    "fill": "none",
+                    "shape-rendering": "crispEdges",
+                    "stroke": "grey",
+                    "stroke-width": "1px"
+                });
 
-                var color = d3.scale.category10()
-                    .domain(["Tax", "Effective Rate", "Marginal Rate"]);
+            var color = d3.scale.category10()
+                .domain(["Tax", "Effective Rate", "Marginal Rate"]);
 
 
 
 
-                var brackets = add_brackets(on_brackets, fed_brackets);
-                brackets = subtract_brackets(brackets, on_personal_amt);
-                brackets = subtract_brackets(brackets, fed_personal_amt);
+            var brackets = add_brackets(on_brackets, fed_brackets);
+            brackets = subtract_brackets(brackets, on_personal_amt);
+            brackets = subtract_brackets(brackets, fed_personal_amt);
 
-                var data2 = [];
+            var data2 = [];
 
-                for (var i = 0; i <= 250000; i += 100) {
-                    data2.push({
-                        "Income": i,
-                        "Tax": taxes_owed(i, brackets),
-                        "Effective Rate": effective_rate(i, brackets),
-                        "Marginal Rate": marginal_rate(i, brackets)
-                    });
-                }
-
-
-
-
-
-                vis.append('svg:path')
-                    .attr('class', 'tax')
-                    .attr('d', lineGenTax(data))
-                    .attr('stroke', color("Tax"))
-                    .attr('stroke-width', 2)
-                    .attr('fill', 'none')
-                    .transition()
-                    .delay(2000)
-                    .duration(2000)
-                    .attr('d', lineGenTax(data2));
-                vis.append('svg:path')
-                    .attr('class', 'effective')
-                    .attr('d', lineGenEff(data))
-                    .attr('stroke', color("Effective Rate"))
-                    .attr('stroke-width', 2)
-                    .attr('fill', 'none')
-                    .transition()
-                    .delay(2000)
-                    .duration(2000)
-                    .attr('d', lineGenEff(data2));
-                vis.append('svg:path')
-                    .attr('class', 'marginal')
-                    .attr('d', lineGenMarg(data))
-                    .attr('stroke', color("Marginal Rate"))
-                    .attr('stroke-width', 2)
-                    .attr('fill', 'none')
-                    .transition()
-                    .delay(2000)
-                    .duration(2000)
-                    .attr('d', lineGenMarg(data2));
+            for (var i = 0; i <= 250000; i += 100) {
+                data2.push({
+                    "Income": i,
+                    "Tax": taxes_owed(i, brackets),
+                    "Effective Rate": effective_rate(i, brackets),
+                    "Marginal Rate": marginal_rate(i, brackets)
+                });
             }
-            InitChart(data);
+
+
+
+
+
+            svg.append('svg:path')
+                .attr('class', 'tax')
+                .attr('d', lineGenTax(data))
+                .attr('stroke', color("Tax"))
+                .attr('stroke-width', 2)
+                .attr('fill', 'none')
+                .transition()
+                .delay(2000)
+                .duration(2000)
+                .attr('d', lineGenTax(data2));
+            svg.append('svg:path')
+                .attr('class', 'effective')
+                .attr('d', lineGenEff(data))
+                .attr('stroke', color("Effective Rate"))
+                .attr('stroke-width', 2)
+                .attr('fill', 'none')
+                .transition()
+                .delay(2000)
+                .duration(2000)
+                .attr('d', lineGenEff(data2));
+            svg.append('svg:path')
+                .attr('class', 'marginal')
+                .attr('d', lineGenMarg(data))
+                .attr('stroke', color("Marginal Rate"))
+                .attr('stroke-width', 2)
+                .attr('fill', 'none')
+                .transition()
+                .delay(2000)
+                .duration(2000)
+                .attr('d', lineGenMarg(data2));
         }
     }
 })
