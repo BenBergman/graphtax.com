8139febb720b00c56d687f95628430a302fea62b
Benjamin Bergman <ben@benbergman.ca>
2016-05-04 07:42:22 -0500

    Remove old code that was part of an older test

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,18 +1,4 @@
 app.controller("MainController", ["$scope", function($scope) {
-    $scope.updateChart = function() {
-        var index = 0;
-        var data = angular.fromJson("[" + $scope.chart_data + "]");
-        var bar_width = 500 / (data.length);
-
-        d3.selectAll('.chart').selectAll('div').remove();
-        d3.select(".chart")
-            .selectAll("div")
-            .data(data)
-            .enter().append("div")
-            .style("width", function(d) { return bar_width + "px"; })
-            .style("height", function(d) { return d + "%"; })
-            .style("left", function(d) { return (index++) * (bar_width + 2) + "px"; });
-    };
     $scope.rawBrackets = {
         "Federal": {
             "income": [
